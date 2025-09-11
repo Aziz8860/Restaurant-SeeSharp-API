fe310afcc527cdb658fa2c81be4b3167f90ab3c6
Aziz <muhaariq@gmail.com>
2025-09-11 07:08:29 +0700

    Add project files.

---
+++ Restaurants.API/Controllers/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get(int count, int minTemp, int maxTemp);
+    }
+
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int count, int minTemp, int maxTemp)
+        {
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(minTemp, maxTemp),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
+        // ngasih tau kalau Address itu punyanya Restaurant Entity (bukan entity terpisah)
+        // You use .OwnsOne when you have an object(like Address) that makes sense only as
+        // part of a parent entity(Restaurant), not independently.
+
+        modelBuilder.Entity<Restaurant>()
+           .HasMany(r => r.Dishes)
+           .WithOne()
+           .HasForeignKey(d => d.RestaurantId);
+        // sets up a standard one-to-many FK relationship (Restaurant → Dishes).
+    }
+}
+
